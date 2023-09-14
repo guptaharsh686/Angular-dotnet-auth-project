ae4f0751cf16fcc9a8c5c05a26d71e628f1654b7
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-14 23:21:07 +0530

    Frontend : Add form validation for login page.

--- AngularAuthUI/src/app/components/login/login.component.ts
+++ AngularAuthUI/src/app/components/login/login.component.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 
+import {FormBuilder,FormGroup,Validators,FormControl} from '@angular/forms'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
   type:string = "password";
   isText:boolean = false;
   eyeIcon:string = "fa fa-eye-slash";
+
+  loginForm!: FormGroup;
+
+  constructor(private fb : FormBuilder)
+  {
+      this.loginForm = fb.group({
+        username: ['',Validators.required],
+        password: ['',Validators.required]
+      })
+  }
+
+  onSubmit(){
+    //console.log("onSubmit Called")
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      //send object to database
+    }
+    else{
+      console.log("form is not valid")
+      //throw error using toster and required field
+      this.validateAllFormFields(this.loginForm);
+      alert("your form is invalid")
+    }
+  }
+
+  private validateAllFormFields(formGroup : FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true});
+      }else if(control instanceof FormGroup){
+        this.validateAllFormFields(control);
+      }
+    })
+  }
+
+
       hideShowPass(){
         this.isText = ! this.isText;
 
