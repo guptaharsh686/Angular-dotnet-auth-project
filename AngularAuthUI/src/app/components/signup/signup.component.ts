18d50810ef703bd1378e85dc7f096d7e916cda4d
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-15 00:27:33 +0530

    Frontend : Added form validation to Signup page

--- AngularAuthUI/src/app/components/signup/signup.component.ts
+++ AngularAuthUI/src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormBuilder,Validators,FormControl} from '@angular/forms'
 
 @Component({
   selector: 'app-signup',
@@ -9,6 +10,41 @@ export class SignupComponent {
   type:string = "password";
   isText:boolean = false;
   eyeIcon:string = "fa fa-eye-slash";
+
+  signupForm !: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.signupForm = fb.group({
+      firstName : ['',Validators.required],
+      lastName  : ['',Validators.required],
+      email     : ['',Validators.required],
+      username  : ['',Validators.required],
+      password  : ['',Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if(this.signupForm.valid){
+      //send to database
+      console.log(this.signupForm.value);
+    }else{
+      console.log("Form is invalid")
+      this.validateAllFormFields(this.signupForm)
+      alert("your form is invalid")
+    }
+  }
+
+  private validateAllFormFields(formGroup : FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      var control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf:true})
+      }
+      else if(control instanceof FormGroup){
+        this.validateAllFormFields(control);
+      }
+    })
+  }
       hideShowPass(){
         this.isText = ! this.isText;
 
