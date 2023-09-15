df3e901e6eea1e5c092f79d41e8a7b2d88073885
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-15 23:31:59 +0530

    Backend : Added AppDbContext and created AuthApiDb through migrations

--- AngularAuthAPI/AngularAuthAPI/Program.cs
+++ AngularAuthAPI/AngularAuthAPI/Program.cs
@@ -1,3 +1,6 @@
+using AngularAuthAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configure DbContext service
+builder.Services.AddDbContext<AppDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConStr"));
+});
 
 var app = builder.Build();
 
