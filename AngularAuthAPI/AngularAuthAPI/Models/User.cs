7f4f4f761428458fe4e5bbe8654b7af410adc6d2
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-09-15 23:14:16 +0530

    Backend : Create User model and add EF package references.

---
+++ AngularAuthAPI/AngularAuthAPI/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AngularAuthAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required] 
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
